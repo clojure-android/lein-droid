f26d20a66def8cbf8210f5abc10d3575b5293ccb
Alexander Yakushev <yakushev.alex@gmail.com>
2012-05-04 10:20:20 +0300

    Push documentation to github pages

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject lein-droid/lein-droid "0.0.1-SNAPSHOT"
-  :description "Plugin for easy Clojure/Android development and deployment"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[robert/hooke "1.1.2"]
-                 [org.clojure/data.zip "0.1.0"]]
-  :min-lein-version "2.0.0"
-  :eval-in-leiningen true)
