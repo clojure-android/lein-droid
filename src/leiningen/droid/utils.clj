ddddf60c56457839e293cceabc665fd59b30edad
Alexander Yakushev <alex@bytopia.org>
2013-04-11 22:26:14 +0300

    Prepare 0.1.0-preview3 release

--- src/leiningen/droid/utils.clj
+++ src/leiningen/droid/utils.clj
@@ -106,15 +106,7 @@
 
 ;; ### General utilities
 
-(defn proj [] (read-project #_"/home/unlogic/work/projects/Android/ActionBarSherlock/project.clj"
-                            #_"/home/unlogic/work/projects/Clojure/android/holmes/project.clj"
-                            #_"/home/unlogic/clojure/android/foreclojure-android/project.clj"
-                            #_"/home/unlogic/clojure/android/nexus-clojure/project.clj"
-                            #_"/home/unlogic/clojure/android/neko/project.clj"
-                            "/home/unlogic/clojure/lein-droid-test/project.clj"
-                            ))
-
-;;  ([path] (init-project (read-project path [:user :default :dev])))
+(defn proj [] (read-project "sample/project.clj"))
 
 (defn get-sdk-platform-path
   "Returns a version-specific path to the Android platform tools."
