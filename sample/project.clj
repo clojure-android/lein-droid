3929ad3fefdb41871d030854596e70c319d360c7
Sergey Vinokurov <serg.foo@gmail.com>
2013-06-17 23:27:08 +0300

    added descriptions of supported options to sample project.clj

--- sample/project.clj
+++ sample/project.clj
@@ -16,7 +16,7 @@
 
   ;; Uncomment this line if your project doesn't use Clojure. Also
   ;; don't forget to remove respective dependencies.
-  ;; java-only true
+  ;; :java-only true
 
   :dependencies [[android/clojure "1.4.0"]
                  [neko/neko "2.0.0-beta3"]]
@@ -27,20 +27,54 @@
                                  ;; key you want to sign APKs with.
                                  ;; :keystore-path "/home/user/.android/private.keystore"
                                  ;; :key-alias "mykeyalias"
+
+                                 ;; You can specify these to avoid
+                                 ;; entering them for each rebuild,
+                                 ;; but generally it's a bad idea.
+                                 ;; :keypass "android"
+                                 ;; :storepass "android"
+
                                  :aot :all}}}
 
   :android {;; Specify the path to the Android SDK directory either
             ;; here or in your ~/.lein/profiles.clj file.
             ;; :sdk-path "/home/user/path/to/android-sdk/"
 
+            ;; Specify this if your project is a library.
+            ;; :library false
+
+            ;; Uncomment this if dexer fails with OutOfMemoryException.
+            ;; :force-dex-optimize true
+
+            ;; Options to pass to dx executable, former is for general
+            ;; java-related options and later is for 'dex task'-specific options.
+            ;; :dex-opts ["-JXmx4096M"]
+            ;; :dex-aux-opts ["--num-threads=2"]
+
+            ;; Proguard config for "droid create-obfuscated-dex" task.
+            ;; :proguard-conf "proguard.cfg"
+            ;; :proguard-opts ["-printseeds"]
+
             ;; Uncomment this line to be able to use Google API.
             ;; :use-google-api true
 
-            ;; Uncomment this if dexer fails with OutOfMemoryException
-            ;; :force-dex-optimize true
-
             ;; Use this property to add project dependencies.
             ;; :project-dependencies [ "/path/to/library/project" ]
+
+            ;; Sequence of external jars to "link" against, e.g. android-support-v4.jar
+            ;; :external-classes-paths ["<sdk-home>/extras/android/support/v4/android-support-v4.jar"]
+
+            ;; sequence of paths where native libraries may be found for packaging
+            ;; :native-libraries-paths ["libs"]
+
+            ;; Target version affects api used for compilation.
             :target-version "10"
-            :aot-exclude-ns ["clojure.parallel"]
+
+            ;; Minimum supported version could be specified as well,
+            ;; its meaning is similar to that in AndroidManifest.xml.
+            ;; :min-version "10"
+
+            ;; Sequence of namespaces that should not be compiled.
+            :aot-exclude-ns ["clojure.parallel" "clojure.core.reducers"]
+
             })
