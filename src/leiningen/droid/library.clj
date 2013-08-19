aa33fb46ce4d7fc13eddf9bcd44ba7f353ae6041
Alexander Yakushev <alex@bytopia.org>
2013-08-19 02:14:29 +0200

    Refactor usage of read-binary-file

--- src/leiningen/droid/library.clj
+++ src/leiningen/droid/library.clj
@@ -19,7 +19,7 @@
      :bytes (.toByteArray
              (with-open [in (io/input-stream f)
                          out (java.io.ByteArrayOutputStream.)]
-              (read-binary-file in out)))
+               (read-binary-file in out)))
      :path (str (io/file "res" (relativize-path res-dir f)))}))
 
 (defn filespecs-hook
@@ -33,35 +33,24 @@
 
 ;; ## Resource extraction
 
-(defn- extract-file
-  "Takes a ZipInputStream and writes its current entry to the
-  specified file."
-  [zip-in out-filename]
-  (with-open [bos (io/make-output-stream out-filename {})]
-    (let [buffer (byte-array 4096)]
-      (loop []
-        (let [r (.read zip-in buffer)]
-          (when-not (= r -1)
-            (.write bos buffer 0 r)
-            (recur)))))))
-
 (defn extract-resources-from-jar
   "Takes a JAR file and extracts all files under res/ directory to
   `out-dir`."
   [jar-file out-dir]
-  (let [zis (ZipInputStream. (io/input-stream jar-file))
-        res-dir (str "res" File/separator)]
-    (loop [found-res false]
-      (if-let [entry (.getNextEntry zis)]
-        (if (and (.startsWith (str entry) res-dir)
-                 (not (.isDirectory entry)))
-          (let [out-file (io/file out-dir (str entry))]
-            (clojure.java.io/make-parents out-file)
-            (extract-file zis out-file)
-            (recur true))
-          (recur found-res))
-        (when found-res
-          (str out-dir File/separator "res"))))))
+  (with-open [zis (ZipInputStream. (io/input-stream jar-file))]
+    (let [res-dir (str "res" File/separator)]
+      (loop [found-res false]
+        (if-let [entry (.getNextEntry zis)]
+          (if (and (.startsWith (str entry) res-dir)
+                   (not (.isDirectory entry)))
+            (let [out-file (io/file out-dir (str entry))]
+              (clojure.java.io/make-parents out-file)
+              (with-open [out (io/make-output-stream out-file {})]
+                (read-binary-file zis out))
+              (recur true))
+            (recur found-res))
+          (when found-res
+            (str out-dir File/separator "res")))))))
 
 (defn extract-resources-from-deps
   "Extracts resources from all project dependencies. Returns a list of
