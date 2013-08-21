22dfb29b88908dd0474e7128108738e96b89c17e
Alexander Yakushev <alex@bytopia.org>
2013-08-22 01:00:20 +0200

    Prepare 0.2.0-beta1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-droid/lein-droid "0.2.0-SNAPSHOT"
+(defproject lein-droid/lein-droid "0.2.0-beta1"
   :description "Plugin for easy Clojure/Android development and deployment"
   :url "https://github.com/alexander-yakushev/lein-droid"
   :license {:name "Eclipse Public License"
