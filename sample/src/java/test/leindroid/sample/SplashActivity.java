b215eead175c178c773e09b0ca4e2b01ab270743
Volker Schlecht <volker@schlecht.org>
2014-10-26 23:14:09 +0100

    Revert to clojure-android/lein-droid baseline

--- sample/src/java/test/leindroid/sample/SplashActivity.java
+++ sample/src/java/test/leindroid/sample/SplashActivity.java
@@ -1,26 +1,9 @@
-package test.leindroid.sample;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.os.AsyncTask;
-import android.util.Log;
-
-import clojure.lang.Symbol;
-import clojure.lang.Var;
-import clojure.lang.RT;
-
 import test.leindroid.sample.R;
 
 public class SplashActivity extends Activity {
 
     private static boolean firstLaunch = true;
-    private String TAG = "Splash";
+    private static String TAG = "Splash";
 
     @Override
     public void onCreate(Bundle bundle) {
@@ -47,43 +30,26 @@ public class SplashActivity extends Activity {
     }
 
     public void proceed() {
-        new ActivityLoaderTask().execute("test.leindroid.sample.MainActivity");
+        startActivity(new Intent("test.leindroid.sample.MAIN"));
         finish();
-    }
-
-    private class ActivityLoaderTask extends AsyncTask<String, Void, Class> {
-    
-        @Override
-        protected Class doInBackground(String... className) {           
-            try {
-                return Class.forName(className[0]);
-            } catch (ClassNotFoundException e) {
-                Log.e(TAG, "Received an exception", e);
-            }
-            return null;
-        }
-    
-        @Override
-        protected void onPostExecute(Class result) {
-            try {
-                startActivity(new Intent(SplashActivity.this, result));
-                finish();
-            }  catch (Exception e) {
-                Log.e(TAG, "Received an exception", e);
-            }
-        }
     }
 
     public void loadClojure() {
         new Thread(new Runnable(){
                 @Override
                 public void run() {
-                    Symbol CLOJURE_MAIN = Symbol.intern("neko.application");
+                    Symbol CLOJURE_MAIN = Symbol.intern("neko.init");
                     Var REQUIRE = RT.var("clojure.core", "require");
                     REQUIRE.invoke(CLOJURE_MAIN);
 
-                    Var INIT = RT.var("neko.application", "init-application");
+                    Var INIT = RT.var("neko.init", "init");
                     INIT.invoke(SplashActivity.this.getApplication());
+
+                    try {
+                        Class.forName("test.leindroid.sample.MainActivity");
+                    } catch (ClassNotFoundException e) {
+                        Log.e(TAG, "Failed loading MainActivity", e);
+                    }
 
                     proceed();
                 }
