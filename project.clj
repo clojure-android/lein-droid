1e7ec8e070b48bcaa1b3a556911f204bed8f4283
Alexander Yakushev <alex@bytopia.org>
2015-02-09 10:05:29 +0200

    Version 0.3.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-droid/lein-droid "0.3.3"
+(defproject lein-droid/lein-droid "0.3.4-SNAPSHOT"
   :description "Plugin for easy Clojure/Android development and deployment"
   :url "https://github.com/clojure-android/lein-droid"
   :license {:name "Eclipse Public License"
